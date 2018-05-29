6619eb7d3e5fd9dd2f7269d449bbdb44469d52d0
meihb <mhb_208@outlook.com>
2018-05-29 22:19:51 +0800

    0529 before sleep

--- baiduindex/redisConn.py
+++ baiduindex/redisConn.py
@@ -3,5 +3,31 @@
 
 import  redis
 
-r = redis.Redis(host='118.25.41.135',port=6379,password='mhbredis')
-print(r.ping())
+
+class myRedisQueue(object):
+    '''
+    redis默认db数量为0-15,可在redis.conf中配置databases 16修改
+    '''
+    def __init__(self,host,port,pwd,db=1):
+        object.__init__(self)
+        self.redis = redis.Redis(host=host,port=port,password=pwd,db=db)
+
+
+    def push(self,name,value):
+        return self.redis.lpush(name,value)
+
+    def pop(self,name):
+        return self.redis.rpush(name)
+
+
+if __name__=='__main__':
+    # print(myRedisQueue)
+    myQueue = myRedisQueue('118.25.41.135',6379,'mhbredis')
+    i = 0
+    while True:
+        if i<10*365*1000:
+            myQueue.push('test',i)
+            i +=1
+        else:
+            break
+    print(myQueue.redis.dbsize())
