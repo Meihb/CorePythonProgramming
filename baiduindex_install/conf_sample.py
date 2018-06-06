cad672170193645cc240f6afda65940e60a2b0be
harbor.M <1253880904@qq.com>
2018-06-06 15:33:28 +0800

    rem conf.py

---
+++ baiduindex_install/conf_sample.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+save_path = r'D:\download'
+
+
+chromeDriver = r'chromedriver.exe'  # chromedriver路径
+tesseract_exe = r'D:\software\dev\Tesseract-OCR\tesseract.exe'  # tesseract.exe路径
+bd_account = '***********'  # 百度账号
+bd_pwd = '************'  # 百度密码
+
+'''mysql'''
+host = '*****'
+port = 3306
+user = '****'
+pwd = '****'
+db = '***'
